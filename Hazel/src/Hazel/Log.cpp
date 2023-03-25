b074d226461c26bf85f561bf844626d5059cb781
wangsl-j <2356880300@qq.com>
2023-03-25 13:03:04 +0800

    add premake5.lua and GnerateProjects

--- Hazel/src/Hazel/Log.cpp
+++ Hazel/src/Hazel/Log.cpp
@@ -8,10 +8,10 @@ namespace Hazel {
 		spdlog::set_pattern("%^[%T] %n: %v%$");
 
 		s_CoreLogger = spdlog::stdout_color_mt("Hazel");
-		s_CoreLogger->set_level(spdlog::level::trace);
+		//s_CoreLogger->set_level(spdlog::level::trace);
 
 		s_ClientLogger = spdlog::stdout_color_mt("APP");
-		s_ClientLogger->set_level(spdlog::level::trace);
+		//s_ClientLogger->set_level(spdlog::level::trace);
 
 	}
 }
